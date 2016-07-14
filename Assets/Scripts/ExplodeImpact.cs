468ed860f8c4995c25f31ff26c8859a77852effe
GalenDrew <galenndrew@gmail.com>
2016-07-14 08:41:28 -0700

    explosion script

---
+++ Assets/Scripts/ExplodeImpact.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeImpact : MonoBehaviour {
+    public float explosionForce;
+    public GameObject collisionParticles;
+    public GameObject explosionParticles;
+    public float hitAllowance;
+
+
+    private Rigidbody rb;
+    private GameObject collisionParticlesStored;
+    private GameObject explosionParticlesStored;
+    private float hitStored;
+    private int count;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter(Collision other)
+    {
+        if (hitStored < hitAllowance)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                hitStored = hitStored + 1;
+                collisionParticlesStored = (GameObject)Instantiate(collisionParticles, transform.position, transform.rotation);
+                collisionParticlesStored.transform.parent = gameObject.transform;
+                Destroy(collisionParticlesStored, 0.5f);
+                other.gameObject.GetComponent<Rigidbody>().AddExplosionForce(explosionForce * 1000, transform.position, 0, 1.0f);
+
+            }
+        }
+
+        if (hitStored == hitAllowance)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                explosionParticlesStored = (GameObject)Instantiate(explosionParticles, transform.position, transform.rotation);
+                gameObject.transform.parent = explosionParticlesStored.transform;
+                Destroy(gameObject);
+                Destroy(explosionParticlesStored, 0.5f);
+                other.gameObject.GetComponent<Rigidbody>().AddExplosionForce(explosionForce * 1200, transform.position, 0, 1.0f);
+            }
+        }
+    }
+
+    }
---
+++ Assets/carDrive.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carDrive : MonoBehaviour {
+    public float DriveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + new Vector3(0,0,DriveSpeed/100);
+	}
+}
